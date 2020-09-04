3309a0081f91ce001dd79982e14d18e68efa6aa2
Itscosmicjay <Itscosmicjay@gmail.com>
2020-09-04 14:07:14 +0100

    methods created

--- My Calculator App/Program.cs
+++ My Calculator App/Program.cs
@@ -6,7 +6,8 @@ namespace My_Calculator_App
     {
         static void Main(string[] args)
         {
-            double num1, num2, num3, num4, num5, num6, num7, num8, num9,a, b,c;
+            double num1, num2, num3, num4, num5, num6, num7, num8, a, b, c;
+            int num9, num10;
 
             Console.WriteLine("Addition");
 
@@ -15,7 +16,8 @@ namespace My_Calculator_App
 
             Console.Write("Enter another number:");
             num2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(num1 + num2);
+
+            Console.WriteLine(Addition(num1, num2));
 
             Console.WriteLine("Subtraction");
 
@@ -24,7 +26,8 @@ namespace My_Calculator_App
 
             Console.Write("Enter another number:");
             num4 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(num3 - num4);
+
+            Console.WriteLine(Substraction(num3, num4));
 
             Console.WriteLine("Multiplication");
 
@@ -33,7 +36,8 @@ namespace My_Calculator_App
 
             Console.Write("Enter another number:");
             num6 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(num5 * num6);
+
+            Console.WriteLine(Multiplication(num5, num6));
 
             Console.WriteLine("Division");
 
@@ -42,13 +46,18 @@ namespace My_Calculator_App
 
             Console.Write("Enter the denominator:");
             num8 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(num7 / num8);
+
+            Console.WriteLine(Division(num7, num8));
 
             Console.WriteLine("Exponential");
 
             Console.Write("Enter a number:");
-            num9 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(Math.Exp(num9));
+            num9 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number:");
+            num10 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Exponential(num9, num10));
 
             Console.WriteLine("Quadratic Equation using formula method");
 
@@ -61,11 +70,46 @@ namespace My_Calculator_App
             Console.Write("Enter the value of c:");
             c = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(-b + Math.Sqrt(b*b -(4*a*c))/2*a );
+            Console.WriteLine(FormularMethod(b, a, c));
 
-           Console.WriteLine(-b - Math.Sqrt(b*b-(4*a*c)) / 2 * a);
+            Console.WriteLine(FormularMethod(b, a, c));
 
             Console.ReadLine();
         }
+
+
+        private static double Addition(double a, double b)
+        {
+            double answer = a + b;
+            return answer;
+        }
+        private static double Substraction(double a, double b)
+        {
+            double answer = a - b;
+            return answer;
+        }
+        private static double Multiplication(double a, double b)
+        {
+            double answer = a * b;
+            return answer;
+        }
+        private static double Division(double a, double b)
+        {
+            double answer = a / b;
+            return answer;
+        }
+        private static int Exponential(int a, int b)
+        {
+            int answer = a^b;
+            return answer;
+        }
+        private static string FormularMethod(double b, double a , double c)
+        {
+            double answer = ((-b) + Math.Sqrt((b * b) - (4 * a * c) / (2 * a)));
+            double answer1 = ((-b) - Math.Sqrt((b * b) - (4 * a * c) / (2 * a)));
+            string Main = "x1 : " +answer + " x2 : " +answer1;
+            return Main;
+        }
     }
 }
+
